8790055b4b93ddccd197396a5f6eb6ecd2d2dc38
Anzifnauzb <91614962+Anzifnauzb@users.noreply.github.com>
2021-09-29 19:47:13 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -91,7 +91,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
+            InlineKeyboardButton('♥️ Share & Support ♥️', url='t.me/sfc2k21')
         ],
         [
             InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
